7d0ef00f4b3264a31cf81e43f49bf9a3879d0e1f
leebyeounggwan <102587669+leebyeounggwan@users.noreply.github.com>
2022-08-15 18:22:36 +0900

    로또 최고 순위와 최저 순위

---
+++ algorithm/src/main/java/org/example/level1/Lotto.java
@@ -0,0 +1,52 @@
+package org.example.level1;
+
+import java.util.Arrays;
+
+public class Lotto {
+
+    public int[] solution(int[] lottos, int[] win_nums) {
+        int[] answer = new int[2];
+        int n = 0;
+        int countZero = 0;
+        int count = 0;
+        while (n < 6){
+            if(lottos[n]==0){countZero++;}
+            for(int i = 0; i < 6; i++){
+                if(lottos[i] == win_nums[n]){
+                    count++;
+                }
+            }
+            n++;
+        }
+
+        answer[0] = count+countZero;
+        answer[1] = count;
+
+        for(int z=0; z<2; z++){
+            if(answer[z] == 6){
+                answer[z] = 1;
+            }else if(answer[z] == 5){
+                answer[z] = 2;
+            }else if(answer[z] == 4){
+                answer[z] = 3;
+            }else if(answer[z] == 3){
+                answer[z] = 4;
+            }else if(answer[z] == 2){
+                answer[z] = 5;
+            }else {
+                answer[z] = 6;
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        Lotto sol = new Lotto();
+
+        int[] numbers1 = {44, 1, 0, 0, 31, 25};
+        int[] numbers2 = {31, 10, 45, 1, 6, 19};
+
+        int[] result1 = sol.solution(numbers1, numbers2);
+
+        System.out.println(Arrays.toString(result1));
+    }
+}
