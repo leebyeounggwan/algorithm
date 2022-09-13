6e41acc3343077765e36c9bbe0f1e70b765adb12
leebyeounggwan <102587669+leebyeounggwan@users.noreply.github.com>
2022-09-13 16:56:10 +0900

    ?뚭퀬由ъ쬁 臾몄젣

---
+++ algorithm/src/main/java/org/example/level1/DivisorPlus.java
@@ -0,0 +1,16 @@
+package org.example.level1;
+
+public class DivisorPlus {
+
+    public int solution(int n) {
+        int answer = 0;
+        for (int i = 1; i <= n/2; i++) {
+            if (n%i==0) {
+                answer += i;
+            }
+        }
+
+        return answer+n;
+    }
+
+}
